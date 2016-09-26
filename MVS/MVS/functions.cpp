6feaa3959ca3b9d2c70247c2668bf527d196e729
Naza Haizer <nazar.haizer@gmail.com>
2016-09-26 22:11:11 +0300

    Really works

--- MVS/MVS/functions.cpp
+++ MVS/MVS/functions.cpp
@@ -1,2 +1,175 @@
 #include "functions.h"
+// Maks
+// I will add testing function later
+// "Standart choice"
+//-----------------------------------------------------------------------------------------------------
+void BSort(int array[], int size)
+{
+	bool flag = true;
+	int i, j;
 
+	int temp;
+
+	for (j = 1;; j++)
+	{
+		for (i = 0; i < size - j; i++)
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				flag = false;
+			}
+
+		if (flag)
+			break;
+
+		flag = true;
+	}
+}
+//*-----------------------------------------------------------------------------------------------------
+// Modification of "boat" sorting
+
+
+void BSort2(int array[], int size) // size - size of array
+{
+	int i, rezerv;				//i - counter for array, rezerv - for saving counter pisition
+	int temp;						//temp - for swap
+	i = 0;
+
+	while (i < size - 1)
+	{
+		if (array[i] > array[i + 1]) // compare
+		{
+			rezerv = i + 1;			// save counter position
+			while ((i >= 0) && (array[i] > array[i + 1]))  // swap while..
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+
+				i--;				// compare elements behind 'i'
+			}
+			i = rezerv;				// return 'i' on position
+		}
+		else
+			i++;					// next elements
+	}
+}
+//-----------------------------------------------------------------------------------------------------
+// Eugene Roman
+
+void InsertionSort(int arrayPtr[], int length) // Insertion sort
+{
+	int temp, // temporary variable for storing the value of the sorted array element
+		item; // the index of the previous element
+	for (int counter = 1; counter < length; counter++)
+	{
+		temp = arrayPtr[counter]; // initialize a temporary variable current value of the array element
+		item = counter - 1; // remember the previous index of the array element
+		while (item >= 0 && arrayPtr[item] > temp) // while the index is not equal to 0, and the previous element of an array greater than the current
+		{
+			arrayPtr[item + 1] = arrayPtr[item]; // permutation of the array elements
+			arrayPtr[item] = temp;
+			item--;
+		}
+	}
+}
+//-----------------------------------------------------------------------------------------------------
+//Andriy Datskiv
+//Quick Sort
+
+void QSort(int array[], int first, int last)
+{
+	if (first < last)//compare first element with last 
+	{
+		int i;//vaiables for indexes
+		int j;
+		int temp;//for swap
+		int point;
+
+		point = array[first];//fixed element of array
+
+		i = first;//left end of array
+		j = last;//right end of array
+
+		while (i < j)
+		{
+			while (array[i] <= point && i < last)
+				i++;
+			while (array[j] >= point && j > first)
+				j--;
+
+			if (i < j)//swapping elements
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+
+		temp = array[first];
+		array[first] = array[j];
+		array[j] = temp;
+
+		//Recursion QuickSort
+		QSort(array, first, j - 1);
+		QSort(array, j + 1, last);
+	}
+}
+
+
+void PrintArray(int array[], int size)
+{
+
+	int i, j, in;
+	cout << "\n\n V masyvi : " << size << "Elementiv \n";
+
+	for (j = 0; j<3; ++j)
+	{
+		switch (j)
+		{
+		case 0:  in = 0;
+			cout << "\n\n Pochatok : \n";
+			break;
+		case 1:  in = size / 2 - 5;
+			cout << "\n\n Seredyna : \n";
+			break;
+		case 2:  in = size - 10;
+			cout << "\n\n Kinets : \n";
+			break;
+		}
+
+		for (i = in; i<in + 10; ++i)
+		{
+			if (i % 5 == 0)
+				cout << endl;
+			cout << setw(15) << array[i];
+		}
+	}
+	cout << endl;
+}
+
+void QuickPackage(int Array[], int size)
+{
+	QSort(Array, 0, N - 1);
+}
+
+void Tester(void(*Function)(int array[], int size1), int Size)  //Tester all methods
+{
+	int Array[N];		//Array
+	srand(time(0));
+	for (int i = 0; i < N; ++i)
+	{
+		Array[i] = rand() % Size;	//Random Array
+	}
+	float start_time = clock(); //Time Start
+	PrintArray(Array, Size);
+	Function(Array, Size);		//Sorting
+	float end_time = clock();	//Time End
+	float search_time = end_time - start_time;
+	PrintArray(Array, Size);
+	cout << "\nTime: ";
+	cout << search_time / CLOCKS_PER_SEC << endl;
+
+}
--- MVS/MVS/main.cpp
+++ MVS/MVS/main.cpp
@@ -1,34 +1,56 @@
 #include "functions.h"
-
 //All functions must be tested in "main"
 void main()
 {
-	const int N = 50000;
-	long i;
-	long Array[N];
-	srand(time(0));
-	long Size[3] = { 10000, 20000, 50000 };
-	char choise;
-	while (true)
-	{
-		cout << "Choose size: " << endl;
-		cout << "1: 10000" << endl;
-		cout << "2: 20000" << endl;
-		cout << "3: 50000" << endl;
+
+	/*
+		long i;
 
 		int size;
-		cin >> size;
 
-		char choise;
+		int choise;
 
-		cout << "Which sort method do you want?" << endl;
-		cout << "1: BSort" << endl;
-		cout << "2: BSort2" << endl;
-		cout << "3: InsertionSort" << endl;
-		cout << "4: QuickSort" << endl;
-		cout << "0: Exit" << endl;
+		char repeat;
 
-		cin >> choise;
+		long Array[N];
+
+		srand(time(0));
+
+		long Size[3] = { 10000, 20000, 50000 };
+
+	float search_timeBSort(0),
+		search_timeB2Sort(0),
+		search_timeInsSort(0),
+		search_timeQuickSort(0);
+
+
+
+
+
+
+	cout << "Choose size: " << endl;
+	cout << "1: 10000" << endl;
+	cout << "2: 20000" << endl;
+	cout << "3: 50000" << endl;
+
+
+		cout << "Your choise: ";
+	cin >> size;
+
+
+
+
+		begin:
+
+	cout << "Which sort method do you want?" << endl;
+	cout << "1: BSort" << endl;
+	cout << "2: BSort2" << endl;
+	cout << "3: InsertionSort" << endl;
+	cout << "4: QuickSort" << endl;
+	cout << "0: Exit" << endl;
+
+	cout << "Your choise: ";
+	cin >> choise;
 
 		switch (size)
 		{
@@ -61,40 +83,130 @@ void main()
 			cout << "Invalid choise!" << endl;
 			return;
 		} break;
-		}
+}
 
-		PrintArray(Array, N);
-		float start_time = clock();
+cout << "\t\tNon-sorted array: " << endl;
 
-		switch (choise)
-		{
-		case 1: BSort(Array, N); break;
+PrintArray(Array, N);
 
-		case 2: BSort2(Array, N); break;
+//line('-', 79);
 
-		case 3: InsertionSort(Array, N); break;
+float start_time = clock();
 
-		case 4: QSort(Array, 0, N - 1); break;
+switch (choise)
+{
+case 1: BSort(Array, N); break;
 
-		case 0: return; break;
-		}
-		PrintArray(Array, N);
-		float end_time = clock();
-		float search_time = end_time - start_time;
+case 2: BSort2(Array, N); break;
 
-		cout << "\nChas sortuvannya: " << search_time / CLOCKS_PER_SEC << "\n";
-		cout << "Do you want to look at another method? Y/N \n";
-		cin >> choise;
-		if (choise == 'Y' || choise == 'y')
-		{
+case 3: InsertionSort(Array, N); break;
 
-		}
-		else
-		{
-			system("pause");
-			return;
-		}
+case 4: QSort(Array, 0, N - 1); break;
+
+case 0: return; break;
+}
+
+float end_time = clock();
+
+cout << "\t\tSorted array: " << endl;
+
+PrintArray(Array, N);
+
+float search_time = end_time - start_time;
+//line('-', 79);
+
+switch (choise)
+{
+	case 1:	 search_timeBSort = end_time - start_time; break;
+	case 2:	 search_timeB2Sort = end_time - start_time; break;
+	case 3:	 search_timeInsSort = end_time - start_time; break;
+	case 4:	 search_timeQuickSort = end_time - start_time; break;
 	}
 
-	system("pause");
+
+if (search_timeBSort != 0) cout << "Time BSort: " << search_timeBSort / CLOCKS_PER_SEC << endl;
+if (search_timeB2Sort != 0) cout << "Time B2Sort: " << search_timeB2Sort / CLOCKS_PER_SEC << endl;
+if (search_timeInsSort != 0) cout << "Time InsertSort: " << search_timeInsSort / CLOCKS_PER_SEC << endl;
+if (search_timeQuickSort != 0) cout << "Time QuickSort: " << search_timeQuickSort / CLOCKS_PER_SEC << endl;
+
+//cout << "\nChas sortuvannya: " << search_time / CLOCKS_PER_SEC << "\n";
+//line('-', 79);
+
+cout << "Do you want to look at another method? Y/N \n";
+cin >> repeat;
+
+if (repeat == 'Y' || repeat == 'y')
+{
+	system("cls");
+	goto begin;
+	}
+	*/
+
+	int Size, choiseSize, Method;
+	cout << "Choose size: " << endl;
+	cout << "1: 10000" << endl;
+	cout << "2: 20000" << endl;
+	cout << "3: 50000" << endl;
+	cin >> choiseSize;
+	switch (choiseSize)
+{
+	case 1:
+	{
+		Size = 10000;
+	} break;
+
+	case 2:
+	{
+		Size = 20000;
+	} break;
+
+	case 3:
+	{
+		Size = 50000;
+	} break;
+
+	default:
+	{
+		cout << "Invalid choise!" << endl;
+		return; 
+	} break;
+}
+	begin:
+	cout << "Which sort method do you want?" << endl;
+	cout << "1: BSort" << endl;
+	cout << "2: BSort2" << endl;
+	cout << "3: InsertionSort" << endl;
+	cout << "4: QuickSort" << endl;
+	cout << "0: Exit" << endl;
+
+	cout << "Your choise: ";
+	cin >> Method;
+	switch (Method)
+	{
+	case 1: 
+		//cout << "Standart choise: ";
+		Tester(BSort, Size); break;
+
+	case 2: //cout << "Modification of 'boat' sorting: ";
+		Tester(BSort2, Size); break;
+
+	case 3: 	//cout << "Insertion sort: ";
+		Tester(InsertionSort, Size); break;
+
+	case 4:
+		//cout << "Quick Sort: ";
+		Tester(QuickPackage, Size); break;
+
+	case 0: return; break;
+	}
+	cout << "Do you want to look at another method? Y/N \n";
+	char repeat;
+	cin >> repeat;
+
+	if (repeat == 'Y' || repeat == 'y')
+	{
+		system("cls");
+		goto begin;
+	}
+	system("pause>>void");
 }
